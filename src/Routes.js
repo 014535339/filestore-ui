f84642efab4c9f1107a7174416fb9db1d5d4f1a5
praveen-sjsu <praveenthakur.sjsu@gmail.com>
2019-10-25 11:43:22 -0700

    Initial Commit

---
+++ src/Routes.js
@@ -0,0 +1,20 @@
+import React, { useState }from "react";
+import {Route, Switch } from "react-router-dom";
+import NotFound from "./containers/NotFound";
+import Home from "./containers/Home";
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
+import AppliedRoute from "./components/AppliedRoute";
+
+
+export default function Routes({ appProps }) {
+  return (
+    <Switch>
+      <AppliedRoute path="/" exact component={Home} appProps={appProps} />
+      <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+      <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
+      { /* Finally, catch all unmatched routes */ }
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
---
+++ src/containers/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Welcome to File Storage Management Application</h1>        
+      </div>
+    </div>
+  );
+}
